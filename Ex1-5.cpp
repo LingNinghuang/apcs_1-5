1ab82914aaa05b5606ca2cf40530bb6fec37a297
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-08 23:46:09 +0800

    Create Ex1-5.cpp

---
+++ Ex1-5.cpp
@@ -0,0 +1,18 @@
+//comments
+//this is a comment
+#include <stdio.h>
+
+int main() {
+    printf("hello world \n"); //this is a comment
+    printf("i am learing c");
+    return 0;
+}
+/* the code below will print the words hello world
+to the screen, and it is amazing */
+#include <stdio.h>
+
+int main() {
+    printf("hello world \n");
+    printf("i an learing c");
+    return 0;
+}
